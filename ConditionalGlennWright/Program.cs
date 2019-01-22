96bc073f6606d83d9ab6dea4294a4c2cfba32c02
Glenn <Glenn@DESKTOP-76J73F3>
2019-01-21 20:09:52 -0500

    Add project files.

---
+++ ConditionalGlennWright/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConditionalGlennWright
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
